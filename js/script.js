5e45e9cc1afeea44fde759b5575b09c7ccde64f2
Arkadiusz Kazmierczak <areeeks@gmail.com>
2024-01-16 14:37:38 +0100

    Add funcionality blog - hidden and show posts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     console.log('Link was clicked!');
   
     /* remove class 'active' from all article links  */
@@ -8,7 +9,7 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
-  
+    this.classList.add('active');
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post.active');
 
@@ -16,10 +17,11 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    const hrefElement = this.getAttribute("href");
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const articleSelector = document.querySelector(hrefElement);
     /* add class 'active' to the correct article */
+    articleSelector.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
